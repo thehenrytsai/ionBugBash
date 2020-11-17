af6cb0cb031ad99d7fb2e5455ee4b8ada297b371
Henry Tsai <henrytsai@outlook.com>
2020-11-16 19:12:45 -0800

    Further simplification of script.

--- src/bugBash.ts
+++ src/bugBash.ts
@@ -1,4 +1,4 @@
-import { IonDid, IonDocumentModel, IonKey, IonNetwork, IonRequest, IonSdkConfig } from "@decentralized-identity/ion-sdk";
+import { IonDid, IonDocumentModel, IonKey, IonNetwork, IonRequest, IonSdkConfig } from '@decentralized-identity/ion-sdk';
 
 // this prints out a create operation request with no key and services
 async function generateIonValues (): Promise<void> {
@@ -6,13 +6,13 @@ async function generateIonValues (): Promise<void> {
      * Add or remove whatever you want here to try and break the sdk or add properties to your did document
      */
 
-    // Sets to testnet.
+    // Sets network to testnet.
     IonSdkConfig.network = IonNetwork.Testnet;
 
     // Generate recovery key and update key for the operation. This is required for the future when you need to update or recover your document
     // index 0 is the public key and index 1 is the private key
-    const recoveryKeys = await IonKey.generateEs256kOperationKeyPair();
-    const updateKeys = await IonKey.generateEs256kOperationKeyPair();
+    const [recoveryKey] = await IonKey.generateEs256kOperationKeyPair();
+    const [updateKey] = await IonKey.generateEs256kOperationKeyPair();
 
     /**
      * This generates an authentication key, and can be added to the document under publicKeys
@@ -32,18 +32,20 @@ async function generateIonValues (): Promise<void> {
     // }
     // const services = [exampleService];
 
-    // This creates a long-form DID.
     const document : IonDocumentModel = {
-    //   publicKeys,
-    //   services
+        // publicKeys,
+        // services
     }
-    const did = IonDid.createLongFormDid({recoveryKey: recoveryKeys[0], updateKey: updateKeys[0], document: document});
+    const input = { recoveryKey, updateKey, document };
+
+    // This creates a long-form DID.
+    const did = IonDid.createLongFormDid(input);
     console.log('==============BELOW IS A LONG-FORM DID==============');
     console.log(JSON.stringify(did));
     console.log('====================================================');
 
     //This creates the create request and prints it out. You can copy and paste this into postman to make the request
-    const request = IonRequest.createCreateRequest({recoveryKey: recoveryKeys[0], updateKey: updateKeys[0], document: document});
+    const request = IonRequest.createCreateRequest(input);
     console.log('==============BELOW IS A CREATE REQUEST=============');
     console.log(JSON.stringify(request, null, 2));
     console.log('====================================================');
