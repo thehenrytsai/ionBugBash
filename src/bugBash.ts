af2a1eeef6b2a51e9be0bbe2a9ab0c812015e00f
Henry Tsai <henrytsai@outlook.com>
2020-11-16 19:05:11 -0800

    Updated bug bash script.

--- src/bugBash.ts
+++ src/bugBash.ts
@@ -1,4 +1,4 @@
-import * as sdk from '@decentralized-identity/ion-sdk';
+import { IonDid, IonDocumentModel, IonKey, IonNetwork, IonRequest, IonSdkConfig } from "@decentralized-identity/ion-sdk";
 
 // this prints out a create operation request with no key and services
 async function generateIonValues (): Promise<void> {
@@ -6,42 +6,47 @@ async function generateIonValues (): Promise<void> {
      * Add or remove whatever you want here to try and break the sdk or add properties to your did document
      */
 
+    // Sets to testnet.
+    IonSdkConfig.network = IonNetwork.Testnet;
 
-    /**
-     * Generate recovery key and update key for the operation. This is required for the future when you need to update or recover your document
-     * index 0 is the public key and index 1 is the private key
-     */
-    const recoveryKeys = await sdk.IonKey.generateEs256kOperationKeyPair();
-    const updateKeys = await sdk.IonKey.generateEs256kOperationKeyPair();
-
+    // Generate recovery key and update key for the operation. This is required for the future when you need to update or recover your document
+    // index 0 is the public key and index 1 is the private key
+    const recoveryKeys = await IonKey.generateEs256kOperationKeyPair();
+    const updateKeys = await IonKey.generateEs256kOperationKeyPair();
 
     /**
      * This generates an authentication key, and can be added to the document under publicKeys
      */
     // const keyId = 'someKeyId';
-    // const purpose = sdk.IonPublicKeyPurpose.Authentication;
-    // const authenticationKey = await sdk.IonKey.generateEs256kDidDocumentKeyPair(keyId, purpose);
-    // const publicKeys = [authenticationKey]
+    // const purpose = IonPublicKeyPurpose.Authentication;
+    // const [publicKey] = await IonKey.generateEs256kDidDocumentKeyPair({id: keyId, purposes: [purpose]});
+    // const publicKeys = [publicKey];
 
     /**
-     * This is a servicce you can add to your document under services
+     * This is a service you can add to your document under services
      */
     // const exampleService = {
     //     id: 'someId',
     //     type: 'website',
     //     serviceEndpoint: 'https://www.ionIsCool.com'
     // }
-    // services = [exampleService]
+    // const services = [exampleService];
 
+    // This creates a long-form DID.
+    const document : IonDocumentModel = {
+    //   publicKeys,
+    //   services
+    }
+    const did = IonDid.createLongFormDid({recoveryKey: recoveryKeys[0], updateKey: updateKeys[0], document: document});
+    console.log('==============BELOW IS A LONG-FORM DID==============');
+    console.log(JSON.stringify(did));
+    console.log('====================================================');
 
-    /**
-     * This creates the create request and prints it out. You can copy and paste this into postman to make the request
-     */
-    const document = {}
-    const request = sdk.IonRequest.createCreateRequest({recoveryKey: recoveryKeys[0], updateKey: updateKeys[0], document: document});
-    console.log('==============THIS IS YOUR REQUEST==============');
+    //This creates the create request and prints it out. You can copy and paste this into postman to make the request
+    const request = IonRequest.createCreateRequest({recoveryKey: recoveryKeys[0], updateKey: updateKeys[0], document: document});
+    console.log('==============BELOW IS A CREATE REQUEST=============');
     console.log(JSON.stringify(request, null, 2));
-    console.log('================================================');
+    console.log('====================================================');
 }
 
 generateIonValues();
